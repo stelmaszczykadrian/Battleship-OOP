a0afca246601d533aceb26dfb3cd12cc1cddd69f
reizergrzegorz <reizergrzegorz@gmail.com>
2022-12-13 11:33:37 +0100

    create new branch

--- Battleship/src/main/java/com/codecool/Board/Board.java
+++ Battleship/src/main/java/com/codecool/Board/Board.java
@@ -1,4 +1,21 @@
 package com.codecool.Board;
 
 public class Board {
+
+    private int boardSize = 10;
+    Square [][] ocean = new Square[boardSize][boardSize];
+
+    public Board(Square[][] ocean) {
+        this.ocean = ocean;
+    }
+    public void board() {
+        for (int i = 0; i<boardSize, i++)   {
+            for (int j = 0, j < boardSize, j++) {
+                System.out.println(board[i][j]);
+            }
+
+        }
+    }
+
+
 }
--- Battleship/src/main/java/com/codecool/Ship/Ship.java
+++ Battleship/src/main/java/com/codecool/Ship/Ship.java
@@ -2,3 +2,4 @@ package com.codecool.Ship;
 
 abstract class Ship {
 }
+
