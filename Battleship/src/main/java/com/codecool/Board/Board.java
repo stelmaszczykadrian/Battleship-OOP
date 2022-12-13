02bab575171e03375b5f6b84e6f2fc2db9ab2e06
stelmaszczykadrian <Tooyota12345@>
2022-12-13 13:37:01 +0100

    Print board 10x10

--- Battleship/src/main/java/com/codecool/Board/Board.java
+++ Battleship/src/main/java/com/codecool/Board/Board.java
@@ -1,21 +1,38 @@
 package com.codecool.Board;
 
+import com.codecool.Ship.Square;
+
 public class Board {
 
-    private int boardSize = 10;
-    Square [][] ocean = new Square[boardSize][boardSize];
+    private final int boardSize = 10;
+    private Square[][] ocean;
 
-    public Board(Square[][] ocean) {
-        this.ocean = ocean;
-    }
-    public void board() {
-        for (int i = 0; i<boardSize, i++)   {
-            for (int j = 0, j < boardSize, j++) {
-                System.out.println(board[i][j]);
+    public void createBoard() {
+        ocean = new Square[boardSize][boardSize];
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                ocean[i][j] = new Square(i, j);
             }
-
         }
     }
 
+    public String toString() {
+        String boardAsString = "";
+        for (int i = 0; i < boardSize; i++) {
+            boardAsString += String.format("%" + 3 + "d", i + 1);
+        }
+        for (int i = 0; i < boardSize; i++) {
+            boardAsString += "\n";
+            boardAsString += (((char) (i + 65)));
+            for (int j = 0; j < boardSize; j++) {
+                if (ocean[i][j] != null) {
+                    boardAsString += " . ";
+                } else {
+                    boardAsString += " " + ocean[i][j] + " ";
+                }
+            }
+        }
+        return boardAsString;
+    }
 
 }
--- Battleship/src/main/java/com/codecool/Game/Display.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.Game;
-
-public class Display {
-}
