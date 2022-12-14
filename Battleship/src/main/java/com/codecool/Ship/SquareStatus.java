a74f5113966d4999ffdb7f8eeda2adfed4ea4b8a
reizergrzegorz <reizergrzegorz@gmail.com>
2022-12-14 09:14:42 +0100

    SquareStatuses class

--- Battleship/src/main/java/com/codecool/Ship/SquareStatus.java
+++ Battleship/src/main/java/com/codecool/Ship/SquareStatus.java
@@ -1,4 +1,20 @@
 package com.codecool.Ship;
 
-public class SquareStatus {
+public enum SquareStatus {
+    EMPTY("■"),
+    SHIP("🛩"),
+    HIT("🔥"),
+    MISS("💩"),
+    SUNK("💀");
+
+    String squareCharacter;
+
+    SquareStatus(String squareCharacter) {
+        this.squareCharacter = squareCharacter;
+    }
+
+    public String getCharacter() {
+        return squareCharacter;
+    }
 }
+
