b34dc8eb7c4ab18285305533aceb4bcdbd174a0e
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2022-12-14 09:21:40 +0100

    Welcome game prints.

--- Battleship/src/main/java/com/codecool/Game/Game.java
+++ Battleship/src/main/java/com/codecool/Game/Game.java
@@ -1,4 +1,19 @@
 package com.codecool.Game;
 
+import com.codecool.Board.Board;
+import com.codecool.Display;
+
 public class Game {
+
+    private final Board board = new Board();
+    private final Display display = new Display();
+
+    public void startGame(){
+        board.createBoard();
+        display.welcomeMessage();
+        display.welcomeShip();
+        display.rulesOfGame();
+        display.mainMenu();
+        display.printBoard(board);
+    }
 }
