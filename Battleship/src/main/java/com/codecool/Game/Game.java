b00586eda6f3fc38e710ccb3fdcc8ef2fa5a1ef0
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2022-12-15 10:44:08 +0100

    playRound feature, isWinning feature - in construction.

--- Battleship/src/main/java/com/codecool/Game/Game.java
+++ Battleship/src/main/java/com/codecool/Game/Game.java
@@ -2,6 +2,7 @@ package com.codecool.Game;
 import com.codecool.Board.Board;
 import com.codecool.Board.BoardFactory;
 import com.codecool.Display;
+import com.codecool.Player.Player;
 
 import java.util.Arrays;
 
@@ -10,6 +11,8 @@ public class Game {
     private final Board board = new Board();
     private final Display display = new Display();
     private final Coordinates coordinates = new Coordinates();
+    private final Player player = new Player();
+    boolean currentPlayer = true;
     Board boardPlayer = BoardFactory.randomPlacement();
 
     public void startGame(){
@@ -17,27 +20,35 @@ public class Game {
         display.welcomeShip();
         display.rulesOfGame();
         display.mainMenu();
-//        display.printBoard(boardPlayer);
         manageMenu();
+        while (!checkForWinner()) {
+            playRound(board, currentPlayer);
+            currentPlayer = !currentPlayer;
+        }
 
 
     }
+
+    public void playRound(Board board, boolean currentPlayer){
+        display.printBoard(board);
+        display.printSeparator();
+        display.printBoard(boardPlayer);
+        display.printSeparator();
+        player.displayTurn(currentPlayer);
+        display.printSeparator();
+        int y = coordinates.getYCoordinates();
+        int x = coordinates.getXCoordinates();
+
+        boolean isShootSuccessful = boardPlayer.isShootSuccessful(y,x);
+        boardPlayer.placeShot(x , y, isShootSuccessful);
+        System.out.println(board);
+    }
+
+
     public void manageMenu(){
         while(true) {
             int menuDecision = input.giveIntInput(4);
             if (menuDecision == 0) {
-                display.printBoard(board);
-                display.printSeparator();
-                display.printBoard(boardPlayer);
-                int y = coordinates.getYCoordinates();
-                int x = coordinates.getXCoordinates();
-
-                boolean isShootSuccessful = boardPlayer.isShootSuccessful(y,x);
-                boardPlayer.placeShot(x , y, isShootSuccessful);
-                System.out.println(isShootSuccessful);
-                display.printBoard(boardPlayer);
-
-
                 break;
             } else if (menuDecision == 3) {
                 display.goodbye();
@@ -48,5 +59,9 @@ public class Game {
                 display.mainMenu();
             }
         }
+    }
+
+    public boolean checkForWinner() {
+        return false;
     }
 }
