bdd570ff15c432a5ea8e9891b17ffd4e0056549e
stelmaszczykadrian <Tooyota12345@>
2022-12-15 10:44:08 +0100

    playRound feature, isWinning feature - in construction.

--- Battleship/src/main/java/com/codecool/Player/Player.java
+++ Battleship/src/main/java/com/codecool/Player/Player.java
@@ -3,7 +3,18 @@ package com.codecool.Player;
 import com.codecool.Board.Board;
 
 public class Player {
+
     Board shootingBoard;
 
+
+
+    public void displayTurn(boolean currentPlayer) {
+        if (currentPlayer){
+            System.out.println("Player 1 turn!");
+        }
+        else {
+            System.out.println("Player 2 turn!");
+        }
+    }
 
 }
